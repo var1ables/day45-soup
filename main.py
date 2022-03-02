ae05f228cceea66db888183d5f18ae738dc76212
var1ables <67993562+var1ables@users.noreply.github.com>
2022-03-02 00:04:53 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open('website.html') as file:
-    contents = file.read()
+# with open('website.html') as file:
+#     contents = file.read()
+#
+# soup = BeautifulSoup(contents, 'html.parser')
+#
+# all = soup.find_all('a')
+# # for tag in all:
+# #     print(tag.get('href'))
+#
+# heading = soup.find(name= 'h1', class_ = 'heading')
+# # print(heading.get('class'))
+#
+# company_url = soup.select(selector='a')
+# print(company_url)
 
-soup = BeautifulSoup(contents, 'html.parser')
+response = requests.get('http://news.ycombinator.com/news')
 
-all = soup.find_all('a')
-# for tag in all:
-#     print(tag.get('href'))
+yc_webpage = response.text
 
-heading = soup.find(name= 'h1', class_ = 'heading')
-# print(heading.get('class'))
+soup = BeautifulSoup(yc_webpage, 'html.parser')
+# print(soup)
+article = soup.find_all(name='a', class_='titlelink')
 
-company_url = soup.select(selector='a')
-print(company_url)
+article_texts = []
+article_links = []
+
+for article_tag in article:
+    article_text = article_tag.get_text()
+    article_texts.append(article_text)
+    article_link = article_tag.get('href')
+    article_links.append(article_link)
+
+article_upvote = [int(score.getText().split()[0]) for score in soup.find_all(name='span', class_='score')]
+
+largest_number = max(article_upvote)
+largest_index = article_upvote.index(largest_number)
+
+
+print(article_texts[largest_index])
+print(article_links[largest_index])
+
+# print(article_texts)
+# print(article_links)
+# print(article_upvote)
